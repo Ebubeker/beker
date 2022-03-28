0f7b1ce077b552347b1d607e1cdf966b8131f23a
Ebubeker <ebubeker1rexha@gmail.com>
2022-03-28 18:13:00 +0200

    fjyfhgitg

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Background} from './HomeStyled';
+
+const Home = () => {
+  return (
+    <Background>Home</Background>
+  )
+}
+
+export default Home
---
+++ src/pages/HomeStyled.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const Background = styled.div`
+  background-image: url("../static/images/background.png");
+  //   background-color: #000;
+`;
